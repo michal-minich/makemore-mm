50fd68d74e0f800c5e314d64fa685074e0842638
Michal Minich <m.minich@cid.com>
2023-07-04 18:01:31 +0200

    probabilities

--- mynn.py
+++ mynn.py
@@ -15,6 +15,11 @@ def readFileSplitByLine(name: str) -> list[str]:
 
 
 def sToI(chars: list[str]) -> dict[str, int]:
+   res =  {s:i+1 for i,s in enumerate(chars)}
+   res['.'] = 0
+   return res;
+
+def sToI2(chars: list[str]) -> dict[str, int]:
    stoi = { '.' : 0 }
    for i, ch in enumerate(chars):
        stoi[ch] = i + 1
@@ -24,11 +29,12 @@ def sToI(chars: list[str]) -> dict[str, int]:
 def iToS(stoi: dict[str, int]) -> dict[int, str]:
     return {i:s for s,i in stoi.items()}
 
-
+device = torch.device("cuda")
 def buildDataSet(words: list[str], 
                  contextSize: int, 
                  stoi: dict[str, int], 
-                 itos: dict[int, str]) -> tuple[Tensor, Tensor]:
+                 itos: dict[int, str],
+                 dvc: str) -> tuple[Tensor, Tensor]:
     X: list[list[int]] = []
     Y: list[int] = []
     for w in words:
@@ -39,7 +45,7 @@ def buildDataSet(words: list[str],
             Y.append(ix)
             #\print(''.join(itos[i] for i in context), '--->', itos[ix])
             context = context[1:] + [ix]
-    return torch.tensor(X), torch.tensor(Y)
+    return torch.tensor(X, device=dvc), torch.tensor(Y, device=dvc)
 
 
 class NetParameters:
@@ -52,16 +58,17 @@ class NetParameters:
 
 
 def makeNetwork(g: torch.Generator, 
-                vocabularySize: int, 
-                embeddingSize: int, 
+                vocabularyLength: int, 
+                embeddingDims: int, 
                 contextSize: int, 
-                hiddenLayerSize: int) -> NetParameters:
+                hiddenLayerSize: int,
+                dvc: str) -> NetParameters:
     np = NetParameters()
-    np.C = torch.randn((vocabularySize, embeddingSize), generator = g)
-    np.W1 = torch.randn((embeddingSize * contextSize, hiddenLayerSize), generator = g)
-    np.b1 = torch.randn(hiddenLayerSize, generator = g)
-    np.W2 = torch.randn((hiddenLayerSize, vocabularySize), generator = g)
-    np.b2 = torch.randn(vocabularySize, generator = g) 
+    np.C = torch.randn((vocabularyLength, embeddingDims), generator = g, device=dvc)
+    np.W1 = torch.randn((embeddingDims * contextSize, hiddenLayerSize), generator = g, device=dvc)
+    np.b1 = torch.randn(hiddenLayerSize, generator = g, device=dvc)
+    np.W2 = torch.randn((hiddenLayerSize, vocabularyLength), generator = g, device=dvc)
+    np.b2 = torch.randn(vocabularyLength, generator = g, device=dvc) 
     np.all = [np.C, np.W1, np.b1, np.W2, np.b2]
     for p in np.all:
         p.requires_grad = True
@@ -76,9 +83,6 @@ class Loss:
 
 class ForwardPassResult(Loss):
     emb: Tensor
-    h: Tensor
-    logits: Tensor
-    loss: Tensor
 
 
 def forwardPass(np: NetParameters,
@@ -94,13 +98,12 @@ def forwardPass(np: NetParameters,
     return r
 
 
-#@torch.no_grad()
 def getLoss(np: NetParameters,
             emb: Tensor,
             y: Tensor) -> Loss:
     r = Loss()
-    embView = emb.view(emb.shape[0], -1)
-    r.h = torch.tanh(embView @ np.W1 + np.b1)
+    hPreActivations = emb.view(emb.shape[0], -1) @ np.W1 + np.b1
+    r.h = torch.tanh(hPreActivations)
     r.logits = r.h @ np.W2 + np.b2
     r.loss = F.cross_entropy(r.logits, y)
     return r
@@ -117,18 +120,27 @@ def updateNet(parameters: list[Tensor],
               iteration: int,
               learningRate: float):
     #learningRate = lrs[iteration]
-    learningRate = 0.1 if iteration < 20_000 else 0.01
+    learningRate = 0.1 if iteration < 80_000 else 0.01
     for p in parameters:
        p.data += -learningRate * p.grad # type: ignore
 
+class Sample:
+    values: list[str]
+    probs: list[float]
+    prob: float
 
 def sample(np: NetParameters,
            g: torch.Generator,
            contextSize: int,
            itos: dict[int, str],
-           countSamples: int):
+           countSamples: int) -> list[Sample]:
+    samples: list[Sample] = []
     for _ in range(countSamples):
-        out = []
+        values: list[int] = []
+        s = Sample()
+        samples.append(s)
+        s.values = []
+        s.probs = []
         context = [0] * contextSize
         while True:
             emb = np.C[torch.tensor([context])] # (1,block_size,d)
@@ -137,9 +149,74 @@ def sample(np: NetParameters,
             counts = logits.exp() # counts, equivalent to next character
             probs = counts / counts.sum(1, keepdim=True) # probabilities for next character
             #probs = F.softmax(logits, dim=1)
-            ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+            ix = int(torch.multinomial(probs, num_samples=1, generator=g).item())
+            s.probs.append(probs[0, ix].item() / (1/27) )
             context = context[1:] + [ix]
-            out.append(ix)
+            values.append(ix)
+            s.values.append(itos[ix])
+            #print(''.join(itos[i] for i in values))
             if ix == 0: 
                 break
-        print(''.join(itos[i] for i in out))
+        s.prob = calcOneProb(s.probs)
+    return samples
+
+
+def sample2(np: NetParameters,
+           g: torch.Generator,
+           contextSize: int,
+           itos: dict[int, str],
+           countSamples: int) -> list[Sample]:
+    samples: list[Sample] = []
+    for _ in range(countSamples):
+        values: list[int] = []
+        s = Sample()
+        samples.append(s)
+        s.values = []
+        s.probs = []
+        probs2: list[float] = []
+        context = [0] * contextSize
+        while True:
+            emb = np.C[torch.tensor([context])] # (1,block_size,d)
+            h = torch.tanh(emb.view(1, -1) @ np.W1 + np.b1)
+            logits = h @ np.W2 + np.b2
+            counts = logits.exp() # counts, equivalent to next character
+            probs = counts / counts.sum(1, keepdim=True) # probabilities for next character
+            #probs = F.softmax(logits, dim=1)
+            ix = int(torch.multinomial(probs, num_samples=1, generator=g).item())
+            probs2.append(probs[0, ix].item())
+            s.probs.append(probs[0, ix].item() / (1/27) )
+            context = context[1:] + [ix]
+            values.append(ix)
+            s.values.append(itos[ix])
+            #print(''.join(itos[i] for i in values))
+            if ix == 0: 
+                break
+        s.prob = calcOneProb(probs2)
+    return samples
+
+
+def calcOneProb(probs: list[float]) -> float:
+    total = probs[0]
+    for p in probs[1:]:
+        total *= p
+    return total / len(probs)
+
+
+def calcProb(np: NetParameters,
+             sample: str,
+             contextSize: int,
+             stoi: dict[str, int]) -> list[float]:
+    values: list[int] = []
+    ps: list[float] = []
+    probs2: list[float] = []
+    context = [0] * contextSize
+    for i in range(len(sample)):
+        emb = np.C[torch.tensor([context])]
+        h = torch.tanh(emb.view(1, -1) @ np.W1 + np.b1)
+        logits = h @ np.W2 + np.b2
+        counts = logits.exp() 
+        probs = counts / counts.sum(1, keepdim=True)
+        ix = stoi[sample[i]]
+        ps.append(probs[0, ix].item() / (1/27) )
+        context = context[1:] + [ix]
+    return ps
